9e9e1d040a6a94ed95573f599b42857087208dd2
NA2AR <nazartroschin@yandex.ru>
2024-10-02 17:16:50 +0300

    commit 2

--- Select-1.sql
+++ Select-1.sql
@@ -1,6 +1,5 @@
-SELECT name, duration FROM tracks
-order by duration desc
-limit 1
+select distinct name, duration FROM tracks
+where duration = (select max(duration) from tracks t)
 
 SELECT name FROM tracks
 where duration > '03%'
@@ -14,17 +13,25 @@ where name not like ' '
 select name from tracks 
 where name like '%мой%' or name like '%my%'
 
-select AVG(duration) from tracks
+select distinct g."name", count(genre_id)  from performersgenre p 
+join genre g on g.id = p.genre_id 
+join performers p2 on p2.id = p.performers_id
+group by g."name" 
+order by count(genre_id)desc 
 
-select count(id) from performers 
+select a."name", avg(t.duration) from album a
+join tracks t on t.album_id = a.id 
+group by a."name" 
+order by avg(t.duration)
 
-select count(*) from tracks
-where id = (select id from album where release between '2019-01-01' and '2021-01-01')
+select count(*) from tracks t
+join album a on a.id = t.album_id 
+where a.id = (select id from album where release between '2019-01-01' and '2021-01-01')
 
 select p2.name from performersalbum p
 join performers p2 on p.performers_id = p2.id 
 join album a on p.album_id = a.id 
-where release not between '2020-01-01' and '2021-01-01'
+where p2.name not like (select p2.name where a."release" between '2020-01-01' and '2021-01-01')
 
 select distinct c2.name from collectiontracks c
 join collection c2 on c.collection_id = c2.id
--- insert.sql
+++ insert.sql
@@ -1,120 +1,70 @@
 insert into  album (name, "release")
-values ('Лимб', '2017-10-11')
-
-insert into  album (name, "release")
-values ('Марабу', '2015-04-17')
-
-insert into  album (name, "release")
-values ('Путник', '1997-12-02')
-
-insert into album  (name, "release")
-values ('Радио Апокалипсис', '2019-02-04')
+values 
+	('Лимб', '2017-10-11'),
+	('Марабу', '2015-04-17'),
+	('Путник', '1997-12-02'),
+	('Радио Апокалипсис', '2020-02-04')
 
 insert into collection ( name, "release")
-values ('Любимое', '2024-10-14')
-
-insert into collection ( name, "release")
-values ('Реп', '2024-03-15')
-
-insert into collection ( name, "release")
-values ('Реп-рок', '2024-05-15')
-
-insert into collection ( name, "release")
-values ('Не включать', '2024-11-26')
-
-insert into collection ( name, "release")
-values ('Старое', '2018-06-28')
+values 
+	('Любимое', '2024-10-14'),
+	('Реп', '2024-03-15'),
+	('Реп-рок', '2024-05-15'),
+	('Не включать', '2024-11-26'),
+	('Старое', '2018-06-28'),
 
 insert into genre (name)
-values ('Рок')
-
-insert into genre (name)
-values ('ПОП')
-
-insert into genre (name)
-values ('Реп')
+values 
+	('Рок'),
+	('ПОП'),
+	('Реп')
 
 insert into performers (name)
-values ('ATL')
-
-insert into performers (name)
-values ('НЛО')
-
-insert into performers (name)
-values ('Зараза')
-
-insert into performers (name)
-values ('КИШ')
-
-insert into  tracks (album_id , name, duration)
-values ('4','мой характер', '00:03:51')
+values 
+	('ATL'),
+	('НЛО'),
+	('Зараза'),
+	('КИШ')
 
 insert into tracks (album_id , name, duration)
-values ('4','Охотник', '00:02:36')
-
-insert into tracks (album_id , name, duration)
-values ('3','Марабу', '00:02:53')
-
-insert into tracks (album_id , name, duration)
-values ('3','Ареола', '00:03:04')
-
-insert into tracks (album_id , name, duration)
-values ('2','Шаман', '00:02:35')
-
-insert into tracks (album_id , name, duration)
-values ('2','Астронавт', '00:02:57')
-
-insert into tracks (album_id , name, duration)
-values ('4','мой характер', '00:03:51')
-
-insert into tracks (album_id , name, duration)
-values ('5','ящик', '00:03:27')
+values 
+	('4','мой характер', '00:03:51'),
+	('4','Охотник', '00:02:36'),
+	('3','Марабу', '00:02:53'),
+	('3','Ареола', '00:03:04'),
+	('2','Шаман', '00:02:35'),
+	('2','Астронавт', '00:02:57'),
+	('4','мой характер', '00:03:51')
+	('5','ящик', '00:03:27')
 
 insert into performersalbum (album_id, performers_id)
-values ('4','4')
-
-insert into performersalbum (album_id, performers_id)
-values ('2','1')
-
-insert into performersalbum (album_id, performers_id)
-values ('3','1')
-
-insert into performersalbum (album_id, performers_id)
-values ('5','1')
+values 
+	('4','4'),
+	('2','1'),
+	('3','1'),
+ 	('5','1')
 
 insert into collectiontracks (tracks_id, collection_id)
-values ('2','1')
+values 
+	('2','1'),
+ 	('3','1'),
+ 	('4','1'),	
+ 	('3','2'),
+ 	('4','2'),
+ 	('6','2'),
+ 	('8','2'),
+ 	('1','4'),
+ 	('5','4'),
+ 	('7','4'),
+ 	('8','4'),
+ 	('8','5')
 
-insert into collectiontracks (tracks_id, collection_id)
-values ('3','1')
+insert into performersgenre (genre_id, performers_id)
+values
+ 	('1','4'),
+ 	('2','1'),
+ 	('2','2'),
+ 	('2','3'),
+ 	('3','1')
 
-insert into collectiontracks (tracks_id, collection_id)
-values ('4','1')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('3','2')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('4','2')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('6','2')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('8','2')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('1','4')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('5','4')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('7','4')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('8','4')
-
-insert into collectiontracks (tracks_id, collection_id)
-values ('8','5')
-
+ 	
